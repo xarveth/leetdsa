f85b7966a7ea876822e1930ce8ffd761d775ec26
Himanshu Yadav <himanshuyadavgdg25@gmail.com>
2026-07-20 16:12:33 +0530

    Time: 5 ms (80.69%), Space: 45.9 MB (72.56%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int og = x;
+        int rev = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x = x / 10;
+        }
+
+        return og == rev;
+    }
+}
